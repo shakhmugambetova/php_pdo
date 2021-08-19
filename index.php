1b1a5689804984f1eb9cf433231cf838b467fc82
zhansayashakh@gmail.com <zhansayashakh@gmail.com>
2021-08-19 15:20:17 +0600

    filling html table from db

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table><tr>
+        <th>id</th>
+        <th>name</th>
+        <th>age</th>
+        <th>salary</th>
+    </tr>
+   
+        <?php
+        try{
+            $pdo  = new PDO('mysql:host=localhost;dbname=worlers;charset=utf8', 'mysql', 'user');
+            $pdo ->setAttribute(PDO:: ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $sql = "SELECT * from workers";
+            $result = $pdo->query($sql);
+             foreach($result as $row){
+                 $data .='<tr>';
+                 $data .='<td>'.$row['id'].'</td>';
+                $data .='<td>'.$row['name'].'</td>';
+
+                 $data .='<td>'.$row['age'].'</td>';
+
+                 $data .='<td>'.$row['salary'].'</td>';
+                 $data .='</tr>';
+            }
+            echo $data;    
+        }
+        catch(PDOException $e){
+            echo 'cannot connect';
+        }
+        ?>
+</table>
+</body>
+</html>
